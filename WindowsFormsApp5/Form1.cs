079767bb72570e84b051eb1864b7728da59707bd
RC0028739 <RC0028739@CRO-ICF07-11>
2018-01-31 13:49:18 +0000

    general fixes

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -12,10 +12,18 @@ namespace WindowsFormsApp5
 {
     public partial class Form1 : Form
     {
-        Dictionary<string, double> prefixes = new Dictionary,string, double>()
-        {
+        Dictionary<string, double> prefixes = new Dictionary<string, double>()
+    {
             {"nm", 1e-9 },
             {"µm", 1e-6 },
+            {"mm", 1e-3 },
+            {"cm", 1e-2 },
+            {"m", 1 },
+            {"km", 1e3 },
+            {"Mm", 1e6 },
+            {"Gm", 1e9 },
+    };
+
         public Form1()
         {
             InitializeComponent();
