b14d7b7713a5e05f8651c0d3f9e41e75fce3c780
RC0028739 <RC0028739@CRO-ICF07-11>
2018-01-31 13:24:07 +0000

    Add project files.

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, double> prefixes = new Dictionary,string, double>()
+        {
+            {"nm", 1e-9 },
+            {"µm", 1e-6 },
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
